42b285fce6c1985888222a9e0928c67c71bc474a
DerekHan <donglak.han@gmail.com>
2022-10-18 11:23:49 -0500

    make a skeleton by init function

--- word-master.js
+++ word-master.js
@@ -1,4 +1,36 @@
 const letters = document.querySelectorAll('.scoreboard-letter');
 const loadingDiv = document.querySelector('.info-bar');
 
-console.log(loadingDiv)
+
+async function init() {
+
+
+function addletter (letter) {
+
+}
+
+//getting key board value
+    document.addEventListener('keydown', function handleKeyPress (event) {
+        const action = event.key;
+
+        console.log(action)
+
+        if (action === 'Enter'){
+            commit();
+        } else if (action === 'Backspace') {
+            backspace();
+        } else if (isLetter(action)) {
+            addLetter(action.toUpperCase());
+            //it's a single letter
+        } else {
+            //do nothing
+        }
+        //return undefined;
+    });
+}
+
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
+}
+
+init();
